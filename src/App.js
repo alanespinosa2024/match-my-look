9eede68b368f9e72dd32d873d4634e2d894307a3
Alan Espinosa <alan.masdescuentos@gmail.com>
2025-03-14 14:49:46 -0600

    Corrección de advertencias en App.js y ajustes en package.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import "./SearchBar.css";
 import Gallery from "./components/Gallery"; // Importar Gallery para mostrar los resultados
