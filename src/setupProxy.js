1399b87e3653bfe6d7d2f68027ab902aa33e9696
Alan Espinosa de los Monteros <alan.masdescuentos@gmail.com>
2025-03-18 13:47:13 -0600

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,11 @@
-module.exports = function (app) {
-  app.set('host', '0.0.0.0'); // Permite que la app sea accesible desde cualquier IP
-  app.set('port', 3000);      // Establece el puerto
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-  });
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    createProxyMiddleware('/api', {
+      target: 'https://www.masdescuentos.mx',
+      changeOrigin: true,
+      secure: true
+    })
+  );
 };
