87df1f2391ef41d1e6504557475e4366117d0ef6
Alan Espinosa de los Monteros <alan.masdescuentos@gmail.com>
2025-03-18 13:57:47 -0600

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
-module.exports = function (app) {
-  app.set('host', '0.0.0.0'); // Permite que la app sea accesible desde cualquier IP
-  app.set('port', 3000);      // Establece el puerto
+// setupProxy.js
+module.exports = function(app) {
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*"); // Permite acceso desde cualquier origen
     next();
   });
 
-  app.get('host', '69.48.200.113'); // Asigna el host correcto
-  app.get('port', 3000);           // Asegúrate de que el puerto esté configurado correctamente
+  app.set('host', '0.0.0.0'); // Asegúrate de que la app sea accesible desde cualquier IP
+  app.set('port', 3000); // Establece el puerto
 };
+
 
