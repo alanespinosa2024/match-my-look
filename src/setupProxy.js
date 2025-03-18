1e7e7d1fcd2eba557e7fc7b8c4320f1e44cc8f0a
Alan Espinosa de los Monteros <alan.masdescuentos@gmail.com>
2025-03-18 14:16:40 -0600

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,31 @@
-// setupProxy.js
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 module.exports = function(app) {
+  // CORS (si es necesario)
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // Permite acceso desde cualquier origen
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Encabezados permitidos
     next();
   });
 
+  // Configura el proxy para redirigir las solicitudes a tu API backend
+  app.use(
+    '/wp-json',
+    createProxyMiddleware({
+      target: 'https://www.masdescuentos.mx',  // El servidor de destino
+      changeOrigin: true,
+      secure: false,  // Desactivar verificación de seguridad SSL si es necesario
+      pathRewrite: {
+        '^/wp-json': '/wp-json', // Redirigir las solicitudes a la ruta correcta en el backend
+      },
+    })
+  );
+
+  // Configura el host y el puerto
   app.set('host', '0.0.0.0'); // Asegúrate de que la app sea accesible desde cualquier IP
-  app.set('port', 3000); // Establece el puerto
+  app.set('port', 3000);       // Establece el puerto
 };
+
 
 
